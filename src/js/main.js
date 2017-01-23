2c974a2dee5c9b0e007396e326fd982e3c39328e
tesselina <tesselina.spaeth@hs-augsburg.de>
2017-01-23 17:49:22 +0100

    doc with jsdoc

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,8 @@
 /**
- * @author    AUTHOR <AUTHOR@hs-augsburg.de>
+ * @author    Wolfgang Kowarschick <kowa@hs-augsburg.de>
  * @copyright 2016
  * @license   CC-BY-NC-SA-4.0
  */
-
-/*requirejs.config
-({
-  baseUrl: 'js', // By default load any modules from directory js
-  paths :
-  {
-    app:      'app',
-    loadjson: 'lib/require/json',
-    text:     'lib/require/text',
-    json:     '../json'
-  }
-});
-
-requirejs
-( ['loadjson!json/init.json', 'app/init'],
-  function(initJSON, init)
-  {
-    init(window, initJSON);
-  }
-);*/
 
 /** @namespace main */
 var
@@ -32,7 +12,7 @@ main =
    * Welcomes the user of the web app by displaying a welcome message
    * that includes his name. The name is fetched from a text input field.
    */
-  sayHello:
+  greet:
     function()
     {
       document.getElementById('text_hello').innerHTML =
@@ -40,25 +20,27 @@ main =
        document.getElementById('section_form').classList.add('hidden');
       document.getElementById('section_welcome').classList.remove('hidden');
     },
- 
+
   /**
    * An keyboard event observer. It tests whether the enter key has been pressed.
-   * If so, the sayHello method is activated. Default reactions of the browser are
+   * If so, the greet method is activated. Default reactions of the browser are
    * disabled.
    * @param {KeyboardEvent} p_event - A standard JavaScript keyboard event object
    *   (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
    */
-  sayHelloOnEnter:
+  greetOnEnter:
     function(p_event)
     {
-      if (p_event.code === 'Enter' || p_event.keyCode === 13)
+      if ((p_event.code === 'Enter' || p_event.keyCode === 13) &&
+          document.activeElement === document.getElementById('input_name')
+         )
       {
         p_event.preventDefault();
         p_event.stopPropagation();
-        main.sayHello();
+        main.greet();
       }
     },
- 
+
   /**
    * Initializes the web app.
    * Is to be called when the web app has been loaded completely.
@@ -67,11 +49,11 @@ main =
     function()
     {
       document.getElementById('button_submit')
-              .addEventListener('click', main.sayHello);
- 
-      window.addEventListener('keydown', main.sayHelloOnEnter);
+              .addEventListener('click', main.greet);
+
+      window.addEventListener('keydown', main.greetOnEnter);
     }
 };
- 
+
 // Call main.init, when you are ready with loading.
 window.addEventListener('load', main.init);
