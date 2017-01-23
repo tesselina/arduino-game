995e33cefa3e4b30773b3707a2164be881395c23
tesselina <tesselina.spaeth@hs-augsburg.de>
2017-01-23 22:45:11 +0100

    set up grunt/watch/npm

--- gruntfile.js
+++ gruntfile.js
@@ -6,42 +6,73 @@
  */
 
 
-module.exports = function(grunt)
-{
+module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig
-  ({
-    pkg: grunt.file.readJSON('package.json'),
- 
-    cssmin:
-    { options: /* https://github.com/jakubpawlowicz/clean-css */
-      { roundingPrecision:  -1,
-        keepSpecialComments: 1
+    ({
+      pkg: grunt.file.readJSON('package.json'),
+
+      cssmin:
+      {
+        options: /* https://github.com/jakubpawlowicz/clean-css */
+        {
+          roundingPrecision: -1,
+          keepSpecialComments: 1
+        },
+        minimize:
+        {
+          files:
+          {
+            'web/css/main.css': ['src/css/main.css']
+          }
+        }
       },
-      minimize:
-      { files:
-        { 'web/css/main.css': ['src/css/main.css']
+
+      uglify:
+      {
+        options: /* https://github.com/gruntjs/grunt-contrib-uglify */
+        {
+          mangle: true,
+          preserveComments: function (p_node, p_comment)
+          { return /@license/.test(p_comment.value); }
+        },
+        minimize:
+        {
+          files: {
+            'web/js/main.js': ['src/js/main.js']
+          }
+        }
+      },
+
+      watch:
+      {
+        css:
+        {
+          files: ['src/css/**/*.css'],
+          tasks: ['cssmin']
+        },
+        js:
+        {
+          files: ['src/js/**/*.js'],
+          tasks: ['uglify']
+        },
+        web:
+        {
+          files: ['web/**/*'],
+          options: {
+            livereload: true,
+            spawn: false
+          }
         }
       }
-    },
- 
-    uglify:
-    { options: /* https://github.com/gruntjs/grunt-contrib-uglify */
-      { mangle:           true,
-        preserveComments: function(p_node, p_comment)
-                          { return /@license/.test(p_comment.value); }
-      },
-      minimize:
-      { files: { 'web/js/main.js': ['src/js/main.js']
-               }
-      }
-    }
-  });
- 
+
+    });
+
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
- 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'cssmin']);
+  grunt.registerTask('default', ['uglify', 'cssmin', 'watch']);
 };
--- src/js/main.js
+++ src/js/main.js
@@ -27,54 +27,50 @@ requirejs
 
 /** @namespace main */
 var
-main =
-{
-  /**
-   * Welcomes the user of the web app by displaying a welcome message
-   * that includes his name. The name is fetched from a text input field.
-   */
-  greet:
-    function()
+  main =
     {
-      document.getElementById('text_hello').innerHTML =
-        'Hello, ' + document.getElementById("input_name").value + '!';
-       document.getElementById('section_form').classList.add('hidden');
-      document.getElementById('section_welcome').classList.remove('hidden');
-    },
+      /**
+       * Welcomes the user of the web app by displaying a welcome message
+       * that includes his name. The name is fetched from a text input field.
+       */
+      greet:
+      function () {
+        document.getElementById('text_hello').innerHTML =
+          'Hello, ' + document.getElementById("input_name").value + '!';
+        document.getElementById('section_form').classList.add('hidden');
+        document.getElementById('section_welcome').classList.remove('hidden');
+      },
 
-  /**
-   * An keyboard event observer. It tests whether the enter key has been pressed.
-   * If so, the greet method is activated. Default reactions of the browser are
-   * disabled.
-   * @param {KeyboardEvent} p_event - A standard JavaScript keyboard event object
-   *   (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
-   */
-  greetOnEnter:
-    function(p_event)
-    {
-      if ((p_event.code === 'Enter' || p_event.keyCode === 13) &&
+      /**
+       * An keyboard event observer. It tests whether the enter key has been pressed.
+       * If so, the greet method is activated. Default reactions of the browser are
+       * disabled.
+       * @param {KeyboardEvent} p_event - A standard JavaScript keyboard event object
+       *   (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
+       */
+      greetOnEnter:
+      function (p_event) {
+        if ((p_event.code === 'Enter' || p_event.keyCode === 13) &&
           document.activeElement === document.getElementById('input_name')
-         )
-      {
-        p_event.preventDefault();
-        p_event.stopPropagation();
-        main.greet();
+        ) {
+          p_event.preventDefault();
+          p_event.stopPropagation();
+          main.greet();
+        }
+      },
+
+      /**
+       * Initializes the web app.
+       * Is to be called when the web app has been loaded completely.
+       */
+      init:
+      function () {
+        document.getElementById('button_submit')
+          .addEventListener('click', main.greet);
+
+        window.addEventListener('keydown', main.greetOnEnter);
       }
-    },
-
-  /**
-   * Initializes the web app.
-   * Is to be called when the web app has been loaded completely.
-   */
-  init:
-    function()
-    {
-      document.getElementById('button_submit')
-              .addEventListener('click', main.greet);
-
-      window.addEventListener('keydown', main.greetOnEnter);
-    }
-};
+    };
 
 // Call main.init, when you are ready with loading.
 window.addEventListener('load', main.init);
